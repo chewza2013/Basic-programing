a8da417b57dbe0096ed717369682130093a74d80
chewza2013 <chewza2013@gmail.com>
2024-07-19 16:26:04 +0700

    ประกาศตัวแปร

--- Ep1/index.ts
+++ Ep1/index.ts
@@ -1,12 +1,12 @@
-const firstname : string = 'Tawanchai'
-console.log(firstname)
+const firstName : string = 'Tawanchai'
+console.log(firstName)
 
 let Y : number = 10
 Y = 20
 console.log(Y)
 
-let อยากกลับบ้าน: boolean = false 
-console.log(อยากกลับบ้าน)
+let iWantToGoHome: boolean = false 
+console.log(iWantToGoHome)
 
 const a: number = 10
 const b: number = 12
